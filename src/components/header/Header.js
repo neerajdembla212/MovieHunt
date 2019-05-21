162ca40dbdc61145201b7cb734cdc97eb6fbe6ca
neeraj_loadshare <neeraj@loadshare.net>
2019-05-21 20:07:48 +0530

    adding css variables and css modules

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Styles from './Header.module.scss';
+
+const Header = (props) => {
+    return (
+        <div className={Styles.Header}>
+
+        </div>
+    )
+}
+
+export default Header;
